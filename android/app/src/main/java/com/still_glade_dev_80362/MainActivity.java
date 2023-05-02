8e44c4fc7b0518f307a5c336f8e7212dd1a6af2d
Crowdbotics <engineering@crowdbotics.com>
2023-05-02 13:33:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/still_glade_dev_80362/MainActivity.java
@@ -0,0 +1,15 @@
+package com.still_glade_dev_80362;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "still_glade_dev_80362";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-glade-dev-80362",
+  url: "https://still-glade-dev-80362.botics.co",
+  api: "https://still-glade-dev-80362.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
